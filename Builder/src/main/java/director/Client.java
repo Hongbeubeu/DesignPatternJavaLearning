29cbae1c3c082d70c499dc2426fe05dbc2f79712
Hongbeubeu <44913755+Hongbeubeu@users.noreply.github.com>
2020-09-14 19:03:48 +0700

    + Add Builder pattern

---
+++ Builder/src/main/java/director/Client.java
@@ -0,0 +1,17 @@
+package director;
+
+import concretebuilder.FastFoodOrderBuilder;
+import product.order.Order;
+import product.type.BreadType;
+import product.type.OrderType;
+import product.type.SauceType;
+import product.type.VegetableType;
+
+public class Client {
+    public static void main(String[] args) {
+        Order order = new FastFoodOrderBuilder()
+                .orderType(OrderType.ON_SITE).orderBread(BreadType.OMELETTE)
+                .orderSauce(SauceType.SOY_SAUCE).orderVegetable(VegetableType.CUCUMBER).build();
+        System.out.println(order);
+    }
+}
