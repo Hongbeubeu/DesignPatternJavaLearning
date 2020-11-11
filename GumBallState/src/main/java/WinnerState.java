46720104af137b20b8d680b1ec4a3b8c8fc9d025
Hongbeubeu <44913755+Hongbeubeu@users.noreply.github.com>
2020-11-12 00:58:18 +0700

    Finished State Pattern

---
+++ GumBallState/src/main/java/WinnerState.java
@@ -0,0 +1,46 @@
+public class WinnerState implements State {
+    GumballMachine gumballMachine;
+
+    public WinnerState(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+
+    @Override
+    public void insertQuarter() {
+        System.out.println("You can't insert a quarter, the machine is sold out");
+    }
+
+    @Override
+    public void ejectQuarter() {
+        System.out.println("You can't eject, you haven't inserted a quarter yet");
+    }
+
+    @Override
+    public void turnCrank() {
+        System.out.println("You turned, but there are no gumballs");
+    }
+
+    @Override
+    public void dispense() {
+
+        gumballMachine.releaseBall();
+        if (gumballMachine.getCount() == 0) {
+            gumballMachine.setState(gumballMachine.getSoldOutState());
+        } else {
+            gumballMachine.releaseBall();
+            System.out.println("YOU'RE A WINNER! you get two gumballs for your quarter");
+            if (gumballMachine.getCount() > 0) {
+                gumballMachine.setState(gumballMachine.getNoQuarterState());
+            }else {
+                System.out.println("Oops, out of gumballs!");
+                gumballMachine.setState(gumballMachine.getSoldOutState());
+            }
+        }
+    }
+
+    public void refill() {}
+
+    public String toString() {
+        return "despensing two gumballs for your quarter, because YOU'RE A WINNER!";
+    }
+}
