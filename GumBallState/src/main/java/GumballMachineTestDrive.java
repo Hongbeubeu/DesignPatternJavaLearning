977a0925af6c09290173565bccdc5d557c267cb6
Hongbeubeu <44913755+Hongbeubeu@users.noreply.github.com>
2020-11-12 01:31:57 +0700

    Beautify main method

--- GumBallState/src/main/java/GumballMachineTestDrive.java
+++ GumBallState/src/main/java/GumballMachineTestDrive.java
@@ -1,17 +1,16 @@
 public class GumballMachineTestDrive {
     public static void main(String[] args) {
-        GumballMachine gumballMachine = new GumballMachine(200);
+        GumballMachine gumballMachine = new GumballMachine(100);
         System.out.println(gumballMachine);
 
         gumballMachine.insertQuarter();
         gumballMachine.turnCrank();
 
         System.out.println(gumballMachine);
-        for (int i = 0; i < 100; i ++) {
+        for (int i = 0; i < 90; i ++) {
             gumballMachine.insertQuarter();
             gumballMachine.turnCrank();
         }
-
 
         System.out.println(gumballMachine);
     }
