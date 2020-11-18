79b69079ae460d23c02bad0549daa68e7e7bd135
Hongbeubeu <44913755+Hongbeubeu@users.noreply.github.com>
2020-11-19 03:58:26 +0700

    Add Pizza Store project - Factory Pattern

---
+++ PizzaStore/src/main/java/ChicagoPizzaStore.java
@@ -0,0 +1,8 @@
+public class ChicagoPizzaStore extends PizzaStore{
+    Pizza createPizza(String item){
+        if (item.equals("cheese")) {
+            return new ChicagoStyleCheesePizza();
+        } else
+            return null;
+    }
+}
